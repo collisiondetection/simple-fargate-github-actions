3ae0e3bb303dc574ea285f2f30c0e7b3c8841186
Dan <info@collisiondetection.biz>
2024-05-14 12:24:09 +0700

    setting up staging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Daniel Baker!');
+  res.send('Hello Daniel John Baker!');
 });
 
 app.listen(80, function () {
