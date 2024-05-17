62eb0991f82693f8da06434b8d994fa29da6bb63
Dan <info@collisiondetection.biz>
2024-05-17 10:59:58 +0700

    testing manual  steps  first

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Daniel John Baker  test feature 10');
+  res.send('Hello Daniel John Baker  test feature 11');
 });
 
 app.listen(80, function () {
